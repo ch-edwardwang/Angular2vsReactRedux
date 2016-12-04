65039d08a488c301bf72355ebe5ee24e544403ad
DrMcDs <edwardwang.94@gmail.com>
2016-12-04 13:10:47 -0800

    Refactor event handler to be more react

--- ReactRedux-table/src/components/PersonInput/PersonInput.js
+++ ReactRedux-table/src/components/PersonInput/PersonInput.js
@@ -7,39 +7,27 @@ class PersonInput extends Component {
     this.onAddPersonClick = this.onAddPersonClick.bind(this);
   }
 
-// TODO: clean up this controller
   onAddPersonClick() {
-    const pIdElement = document.getElementById('personId');
-    const nameElement = document.getElementById('name');
-    const phoneNumberElement = document.getElementById('phone');
-    const emailElement = document.getElementById('email');
-
     this.props.addPerson({
-      id: pIdElement.value,
-      name: nameElement.value,
-      phoneNumber: phoneNumberElement.value,
-      email: emailElement.value
+      id: this.pId.value,
+      name: this.name.value,
+      phone: this.phone.value,
+      email: this.email.value
     });
 
-    pIdElement.value = '';
-    nameElement.value = '';
-    phoneNumberElement.value = '';
-    emailElement.value = '';
-
-    nameElement.focus();
-  }
-
-  componentDidMount() {
-    document.getElementById('name').focus();
+    this.pId.value = '';
+    this.name.value = '';
+    this.phone.value = '';
+    this.email.value = '';
   }
 
   render() {
     return (
       <div>
-        <input id="personId" type="text" placeholder="Person Id" />
-        <input id="name" type="text" placeholder="Name" />
-        <input id="phone" type="text" placeholder="Phone Number" />
-        <input id="email" type="text" placeholder="Email" />
+        <input ref={(r) => { this.pId = r; }} type="text" placeholder="Person Id" />
+        <input ref={(r) => { this.name = r; }} type="text" placeholder="Name" />
+        <input ref={(r) => { this.phone = r; }} type="text" placeholder="Phone Number" />
+        <input ref={(r) => { this.email = r; }} type="text" placeholder="Email" />
         <button onClick={this.onAddPersonClick}>Add Person</button>
       </div>
     );
